59e6c8a070ee56b8fb5c074ba6803b56664650aa
avinash-sharma-121 <kumaravinashsharma81@gmail.com>
2026-03-01 04:44:36 +0530

    created iot_rule_for sitewise

--- aws_iot_infra/iot_main.tf
+++ aws_iot_infra/iot_main.tf
@@ -1,4 +1,3 @@
-
 resource "aws_iot_thing_group" "motors_via_tf"{
     name = "motors_via_tf"
 
@@ -83,3 +82,38 @@ resource "aws_iot_thing_principal_attachment" "att" {
 }
 
 
+resource "awscc_iot_topic_rule" "test_via_tf" {
+  rule_name = "test_via_tf"
+
+  topic_rule_payload = {
+    sql              = "SELECT * FROM 'robots/LHM-Rack/+'"
+    aws_iot_sql_version = "2015-10-08"
+    rule_disabled    = false
+
+    actions = [
+      {
+        iot_site_wise = {
+          role_arn = "arn:aws:iam::385163247062:role/CdkStack-sitewiseiotruleingestfromiotserviceroleBFC-oYbi9gSqjadp"
+
+          put_asset_property_value_entries = [
+            {
+              property_alias = "robots/LHM-Rack/$${topic(3)}"
+
+              property_values = [
+                {
+                  value = {
+                    double_value = "$${temperature_c}"
+                  }
+
+                  timestamp = {
+                    time_in_seconds = "$${timestamp}"
+                  }
+                }
+              ]
+            }
+          ]
+        }
+      }
+    ]
+  }
+}
